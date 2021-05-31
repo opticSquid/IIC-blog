f199a1a44d4fddc02d31f987b3c4ba996a621f11
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-05-31 09:03:17 +0530

    banner

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -12,10 +12,25 @@ function Article() {
     let toWrite = Data.filter(filter);
     return toWrite[0];
   };
+  console.log(article());
   const classes = useStyles();
   return (
-    <Paper className={classes.root}>
+    <div className={classes.root}>
       <Grid container spacing={1}>
+        <div className={classes.banner}>
+          <Grid container
+            direction="column"
+            justify="center"
+            alignItems="center" className={classes.tint}>
+            <Typography variant="h4" className={classes.title}>
+              {article().Title}
+            </Typography>
+          <Typography variant="body1" className={classes.author}>
+            - {article().Author}
+          </Typography>
+          </Grid>
+          
+        </div>
         <Grid item xs={12}>
           <Typography variant="h6" className="Articletitle">
             {article().Title}
@@ -29,10 +44,11 @@ function Article() {
         </Grid>
         <Grid item xs={12} className={classes.body}>
           {/* Setting inner HTML treating article.Body as a HTML document. */}
+          
           <Typography variant="subtitle2" dangerouslySetInnerHTML={{__html:article().Body}} />
         </Grid>
       </Grid>
-    </Paper>
+    </div>
   );
 }
 
@@ -40,13 +56,37 @@ export default Article;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    margin: theme.spacing(1),
-    padding: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    // padding: theme.spacing(1),
+    // paddingLeft : theme.spacing(13),
+    // paddingRight : theme.spacing(13),
   },
   author: {
-    color: grey[500],
+    color: grey[400],
   },
   body: {
     textAlign: "justify",
   },
+  banner:{
+    width: "100%",
+    height: "60vh",
+    backgroundImage : "url('https://source.unsplash.com/random')",
+    backgroundRepeat : "no-repeat",
+    backgroundSize : "cover",
+    backgroundPosition : "center",
+  },
+  tint:{
+    width: "100%",
+    height: "100%",
+    backgroundColor : "rgba(0,0,0,.7)",
+    paddingLeft : theme.spacing(6),
+    paddingRight : theme.spacing(6),
+  },
+  title:{
+    fontFamily : "Montserrat",
+    color : grey[300],
+    fontWeight : 500,
+    textAlign : "center",
+    // color : "#fff",
+  }
 }));
