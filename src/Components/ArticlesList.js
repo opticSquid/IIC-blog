c1b287eefcb3b46e6b7655ee700fcef50e661272
Vikas Kumar Saw <vikasvks0112@gmail.com>
2021-06-02 21:58:59 +0530

    removed unused vars

--- src/Components/ArticlesList.js
+++ src/Components/ArticlesList.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import {
   Paper,
   Grid,
@@ -6,8 +6,9 @@ import {
   makeStyles,
   useMediaQuery,
   Box,
-  Grow,
+  // Grow,
 } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 //import { grey } from "@material-ui/core/colors";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import { Link } from "react-router-dom";
